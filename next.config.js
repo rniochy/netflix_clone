da90c3b34e88c72e8a50f15033a6eb3602bec6e5
rniochy <lniochy@gmail.com>
2022-04-25 00:52:18 +0100

    add new features o enviroment variables

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    env: {
+        BASEURL: 'http://api.themoviedb.org/3',
+        API_KEY: '8e821c6b841b6f1c86cdaf03d869d0fe',
+    },
+  }
--- pages/requests.js
+++ pages/requests.js
@@ -1,5 +1,4 @@
-const API_KEY = '8e821c6b841b6f1c86cdaf03d869d0fe';
-
+const API_KEY = process.env.API_KEY 
  const requests = {
         fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
         fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=123`,
